60b4ddc2c7603c94a6801c9dcb8d702c3d11eb86
Ryan Hang <koushizon@gmail.com>
2021-10-23 12:09:59 +0800

    Update 0973. K Closest Points to Origin.py

--- 0973. K Closest Points to Origin.py
+++ 0973. K Closest Points to Origin.py
@@ -1,9 +1,10 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         heap = []
-        res = []
         for p in points:
-            heappush(heap, (p[0] * p[0] + p[1] * p[1], p))
-        for i in range(k):
-            res.append(heappop(heap)[1])
-        return res
+            dist = -(p[0] * p[0] + p[1] * p[1])
+            if len(heap) == k:
+                heappushpop(heap, (dist, p))
+            else:
+                heappush(heap, (dist, p))
+        return [p for dist, p in heap]
